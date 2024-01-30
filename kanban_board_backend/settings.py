49249a1934943618fc76df6e25a3f41c5a99341d
simon <simonesders2003@gmail.com>
2024-01-30 23:01:00 +0100

    changes

--- kanban_board_backend/settings.py
+++ kanban_board_backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'todo_list',
     'rest_framework',
+    'corsheaders',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
